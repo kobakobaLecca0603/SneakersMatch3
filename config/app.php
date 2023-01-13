989e38db5542e3ae0df47c42e580fbf99b5b1b58
kobakobaLecca0603 <lingtaixiaolin1@gmail.com>
2023-01-13 17:36:57 +0900

    .env and cloudinary

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'WindsMatch'),
+    'name' => env('APP_NAME', 'SneakersMatch'),
 
     /*
     |--------------------------------------------------------------------------
